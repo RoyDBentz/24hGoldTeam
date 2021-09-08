500ef2dd5ff12115c0db2670ea0d72f3a371d7a4
RoyDBentz <roydbentz@gmail.com>
2021-09-07 21:40:26 -0400

    Comments, maybe

---
+++ GoldTeam.Models/CommentCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldTeam.Models
+{
+    public class CommentCreate
+    {
+        [MaxLength(400)]
+        public string Text { get; set; }
+    }
+}
---
+++ GoldTeam.Services/CommentService.cs
@@ -0,0 +1,76 @@
+﻿using GoldTeam.Data;
+using GoldTeam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldTeam.Services
+{
+    public class CommentService
+    {
+        private readonly Guid _userId;
+
+        public CommentService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateComment(CommentCreate comment)
+        {
+            var entity = new Comment()
+            {
+                AuthorId = _userId,
+                Text = comment.Text,               
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Comments.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CommentListItem> GetComments()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Comments.Where(a => a.AuthorId == _userId).Select(a => new CommentListItem
+                {
+                    Id = a.Id,
+                    Text = a.Text
+                });
+
+                return query.ToArray();
+            }
+        }
+
+        public CommentDetails GetCommentById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Comments.Single(a => a.Id == id && a.AuthorId == _userId);
+                return new CommentDetails
+                {
+                    Id = entity.Id,                    
+                    Text = entity.Text
+                };
+            }
+        }
+
+        public bool UpdateComment(CommentEdit comment)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Comments.Single(a => a.AuthorId == _userId);
+                
+                entity.Text = comment.Text;
+
+                return ctx.SaveChanges() == 1;
+
+            }
+        }
+    }
+}
+}
